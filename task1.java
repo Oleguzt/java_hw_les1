d13161b6e0c7287cb86bd88c0f07bddc7c2f0217
oleg <oleguzt@gmail.com>
2023-03-31 20:53:27 +0500

    task1 done

---
+++ task1.java
@@ -0,0 +1,14 @@
+//   1. Написать метод, принимающий на вход два целых числа и проверяющий,
+//   что их сумма лежит в пределах от 10 до 20 (включительно), если да – вернуть true, 
+// в противном случае – false.
+
+public class task1 {
+    public static boolean SumOfValue (int a, int b) {
+        return ((a+b)>=10 && (a+b) <= 20 );
+    }
+    public static void main(String[] args) {
+        boolean result = SumOfValue(15,7);
+        System.out.println(result);
+    }
+    
+}
