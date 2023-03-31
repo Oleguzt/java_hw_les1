8d0afec3824935f3fcc8e4fc83c6985c0600c8a8
oleg <oleguzt@gmail.com>
2023-03-31 20:54:14 +0500

    task4 done

---
+++ task4.java
@@ -0,0 +1,15 @@
+package hw1;
+// проверить, является ли год високосным. если да - return true
+// год является високосным, если он делится на 4, но не делится на 100, 
+// но делится на 400
+public class task4 {
+    private static boolean isLeapYear(int year) {
+        
+        return ((year%4==0) && (year%100>0)) || (year%400==0);
+    }
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(1988));
+        
+    }
+    
+}
